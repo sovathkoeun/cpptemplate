155f4ffab612e87cf750b57131bb3e14205cb506
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-17 22:52:46 +0700

    add new code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-//one line comment
-
-//this brings in the iostream library
 #include <iostream>
 
-int main(){
+int main() {
+    int first_number = {3};
+    int second_number = {4};
+    std:: cout << "First number :" << first_number << std::endl;
+    std:: cout << "Second number :" << second_number << std::endl;
 
-    std::cout << "Hello World" << std::endl;
+
+    int sum = first_number + second_number;
+    std:: cout << "sum : " << sum << std::endl;
+
 
     return 0;
 }
