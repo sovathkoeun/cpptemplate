25494296613e3f1aee3177b83086d4528ad3fee9
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-17 22:59:01 +0700

    add new

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int addNumber(int first_progam, int scond_program) {
+    int result = first_progam + scond_program;
+    return result;
+}
 int main() {
     int first_number = {3};
     int second_number = {4};
@@ -9,6 +13,13 @@ int main() {
 
     int sum = first_number + second_number;
     std:: cout << "sum : " << sum << std::endl;
+
+    int result = addNumber(25,50);
+    std:: cout << "result : " << result << std::endl;
+
+    sum = addNumber(22,30);
+    std:: cout << "sum : " << sum << std::endl;
+    
 
 
     return 0;
